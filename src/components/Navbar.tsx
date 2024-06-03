1a5367a887ce506adfc1513d307c28d0ad4d3b0b
Talatam Vydhika <talatam.vydhika@gmail.com>
2024-06-03 11:04:04 +0530

    updates.tsx changes
    made the changes and added spacing between the navbar elements. Imported the required pictures

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,43 +6,50 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 import Image from "next/image";
 
-function Navbar({ className }: { className?: string}) {
+function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
-  return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
-        <Menu setActive={setActive}>
-            <HoveredLink href={"#"}>
-            <MenuItem setActive={setActive} active={active} item="Home">
-            </MenuItem>
-            </HoveredLink>
-            <HoveredLink href={"#"}>
-            <MenuItem setActive={setActive} active={active} item="About Us">
-            </MenuItem>
-            </HoveredLink>
-            <MenuItem setActive={setActive} active={active} item="Teams">
-                <div className="flex flex-col space-y-4 text-sm">
-                    <HoveredLink href="/teams">Executive Body</HoveredLink>
-                    <HoveredLink href="/teams">Core Team</HoveredLink>
-                    <HoveredLink href="/teams">StarGazers</HoveredLink>
-                    <HoveredLink href="/teams">ROBusta</HoveredLink>
+    return (
+        <div className={cn("fixed top-10 inset-x-0 max-w-3xl mx-auto z-50", className)}>
+            <Menu setActive={setActive}>
+                <div className="mx-4 px-4">
+                    <HoveredLink href={"#"}>
+                        <MenuItem setActive={setActive} active={active} item="Home" />
+                    </HoveredLink>
                 </div>
-            </MenuItem>
-            <MenuItem setActive={setActive} active={active} item="Events">
-                <div className="flex flex-col space-y-4 text-sm">
-                    <HoveredLink href="/teams">Event A</HoveredLink>
-                    <HoveredLink href="/teams">Event B</HoveredLink>
-                    <HoveredLink href="/teams">Event C</HoveredLink>
-                    <HoveredLink href="/teams">Event D</HoveredLink>
+                <div className="mx-4 px-4">
+                    <HoveredLink href={"#"}>
+                        <MenuItem setActive={setActive} active={active} item="About Us" />
+                    </HoveredLink>
                 </div>
-            </MenuItem>
-            <MenuItem setActive={setActive} active={active} item="Sponsor Us">
-            </MenuItem>
-            <MenuItem setActive={setActive} active={active} item="Contact Us">
-            </MenuItem>
-        </Menu>
-    </div>
-  )
+                <div className="mx-4 px-4">
+                    <MenuItem setActive={setActive} active={active} item="Teams">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/teams">Executive Body</HoveredLink>
+                            <HoveredLink href="/teams">Core Team</HoveredLink>
+                            <HoveredLink href="/teams">StarGazers</HoveredLink>
+                            <HoveredLink href="/teams">ROBusta</HoveredLink>
+                        </div>
+                    </MenuItem>
+                </div>
+                <div className="mx-4 px-4">
+                    <MenuItem setActive={setActive} active={active} item="Events">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/teams">Event A</HoveredLink>
+                            <HoveredLink href="/teams">Event B</HoveredLink>
+                            <HoveredLink href="/teams">Event C</HoveredLink>
+                            <HoveredLink href="/teams">Event D</HoveredLink>
+                        </div>
+                    </MenuItem>
+                </div>
+                <div className="mx-4 px-4">
+                    <MenuItem setActive={setActive} active={active} item="Sponsor Us" />
+                </div>
+                <div className="mx-4 px-4">
+                    <MenuItem setActive={setActive} active={active} item="Contact Us" />
+                </div>
+            </Menu>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
