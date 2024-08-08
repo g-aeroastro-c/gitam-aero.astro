383f4d30848d63b4ab7b441bf7fe4351a973d34d
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-08-08 14:56:27 +0530

    build-fix

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -43,7 +43,7 @@ export function SignupFormDemo() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Send &rarr;
+          {`Send &rarr`};
           <BottomGradient />
         </button>
 
